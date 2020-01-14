b1d091515e6920ed2a62f3d6f1e3c87201b7acf5
Lail <ccwangshibo@163.com>
2020-01-14 23:52:48 +0800

    v-0114:login

--- rq/static/js/main.js
+++ rq/static/js/main.js
@@ -1,4 +1,4 @@
-//登录注册的显示与隐藏
+//1. 登录注册的显示与隐藏
 let l_flag=true;
 let r_flag=true;
 let login=document.getElementsByClassName("login-content");
@@ -25,7 +25,7 @@ let reg_show=function(){
 		r_flag=true;
 	}
 }
-//菜单的显示与隐藏
+//2. 菜单的显示与隐藏
 let menu_item=document.getElementsByClassName("menu-item");
 let menu_details=document.getElementsByClassName("menu-details");
 for(let i=0; i<menu_item.length;i++){
@@ -43,3 +43,51 @@ for(let i=0; i<menu_item.length;i++){
 	})
 }
 
+// 3. 用户名密码格式验证
+let user=document.getElementById("user");
+let upwd=document.getElementById("upwd");
+// 验证用户名
+user.addEventListener("focus",function(){
+	let div=this.parentNode.children[1]
+	div.innerHTML="用户名为8-12为数字或字母";
+	div.className="vali";
+})
+user.addEventListener("blur",function(){
+	let reg=/^\w{8,12}$/;
+	let successMsg="用户名格式正确";
+	let failMsg="用户名格式错误"
+	vali.call(this,reg,successMsg,failMsg);
+})
+// 验证密码
+upwd.addEventListener("blur",function(){
+	let reg=/^\w{6,8}$/;
+	let successMsg="密码格式正确";
+	let failMsg="密码格式错误"
+	vali.call(this,reg,successMsg,failMsg);
+})
+upwd.addEventListener("focus",function(){
+	let div=this.parentNode.children[1]
+	div.innerHTML="密码为6-12为数字或字母";
+	div.className="vali";
+})
+// 验证函数封装
+function vali(reg,successMsg,failMsg){
+	let div=this.parentNode.children[1];
+	let bool=reg.test(this.value);
+	if(bool==true){
+		div.className="vali_success";
+		div.innerHTML=successMsg;
+		return true;
+	}else{
+		div.className="vali_fail";
+		div.innerHTML=failMsg;
+	}
+}
+lg_btn.addEventListener("click",function(){
+	if(vali.call(upwd,/^\w{6,8}$/)==true
+	&&vali.call(user,/^\w{8,12}$/)==true){
+		alert("登录成功")
+	}else{
+		alert("输入格式有误")
+	}
+})
